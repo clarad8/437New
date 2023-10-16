788a99d923bf98639e857f6a9adfc51b12c1c362
clarad8 <d.wanying@wustl.edu>
2023-10-16 17:16:42 -0500

    google font removed

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,12 +1,13 @@
 import { authOptions } from "../../pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
 import "./globals.css";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/";
 import SessionProvider from "./SessionProvider";
 import Login from "./Login";
 import Home from "./page";
-import TutorCourse from "@/pages/tutor-course";
-const inter = Inter({ subsets: ["latin"] });
+import TutorCourse from "../../pages/tutor-course";
+import React from "react";
+// const inter = Inter({ subsets: ["latin"] });
 
 export default async function RootLayout({
   children,
@@ -16,7 +17,7 @@ export default async function RootLayout({
   const session = await getServerSession(authOptions);
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <SessionProvider session={session}>
           {!session ? (
             <Login />
