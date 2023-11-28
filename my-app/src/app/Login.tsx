a420b813f3bc4165fde0bcea6082a6e3a6f09a9e
victoriacheungwustl <71216093+victoriacheungwustl@users.noreply.github.com>
2023-11-28 12:33:40 -0600

    login page ui done

--- my-app/src/app/Login.tsx
+++ my-app/src/app/Login.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect, CSSProperties } from "react";
-import { Typography, Box, Button, Modal } from "@mui/material";
+import { Typography, Box, Button, Modal, Grid } from "@mui/material";
 import {
   signInWithPopup,
   GoogleAuthProvider,
@@ -118,94 +118,125 @@ const Login = () => {
   };
 
   const containerStyle: CSSProperties = {
-    backgroundImage: "url('https://img.freepik.com/premium-photo/blue-cardboard-background-flat-lay-top-view_164357-2984.jpg')", // Replace 'YOUR_IMAGE_URL' with your actual image URL
-    backgroundSize: "cover",
-    backgroundPosition: "center",
     minHeight: "100vh",
+    display: "flex",
+    flexDirection: "row",
+  };
+
+  const leftContainerStyle: CSSProperties = {
+    flex: 1,
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
+    padding: "100px",
+  };
+
+  const rightContainerStyle: CSSProperties = {
+    flex: 1,
+    backgroundImage:
+      "url('https://img.freepik.com/free-vector/webinar-concept-illustration_114360-4874.jpg?w=1480&t=st=1701195212~exp=1701195812~hmac=d97e052cab174955557e86ece8a16b8604be08e722402991a83e5c62d2825f1a')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white",
+    textShadow: "2px 2px black",
+  };
+
+  const buttonContainerStyle: CSSProperties = {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: "20px",
   };
 
   return (
     <div style={containerStyle}>
-    <div style={{ textAlign: "center", padding: "20px", color: "white", textShadow: "2px 2px black" }}>
-    <div style={{ fontFamily: "Georgia", fontSize: "3rem" }}>
-  CS Tutor Match
-</div>
+    <div style={leftContainerStyle}>
+    <Typography variant="h2" style={{ fontWeight: '600' }} gutterBottom>
+          CS <span style={{ color: '#2196f3' }}>Tutor</span>Match
+        </Typography>
+      <Typography variant="body1" style={{ fontWeight: 'normal', textAlign: 'center'  }} gutterBottom>
+      Are you a WashU student seeking personalized assistance in your CS classes? Look no further! CS TutorMatch is your go-to platform for connecting with peer tutors who excel in computer science.
+      </Typography>
 
-        {/* ... other components */}
-        <div style={{ marginTop: "20px" }}>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleGoogleSignUp}
-              style={{ background: "white", color: "#1976D2" }}
-            >
-              Sign up with Google
-            </Button>
-          </div>
-          <div style={{ marginTop: "20px" }}>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleGoogleLogin}
-              style={{ background: "white", color: "#1976D2" }}
-            >
-              Sign in with Google
-            </Button>
-          </div>
-      
-      <Modal
-        open={isVerificationPopupOpen}
-        onClose={() => setVerificationPopupOpen(false)}
+      {/* <Typography variant="subtitle1" style={{ marginBottom: '10px', fontWeight: '600' }}>
+            Get Started Today!
+          </Typography> */}
+
+      <div style={buttonContainerStyle}>
+        
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleGoogleSignUp}
+            style={{ background: "white", color: "#1976D2", marginRight: "10px" }}
+          >
+            Sign up with Google
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleGoogleLogin}
+            style={{ background: "#2196f3", color: "white" }}
+          >
+            Sign in with Google
+          </Button>
+        </div>
+    </div>
+    <div style={rightContainerStyle}>
+      {/* Optional: Add any content you want on the right side */}
+    </div>
+    <Modal
+      open={isVerificationPopupOpen}
+      onClose={() => setVerificationPopupOpen(false)}
+    >
+      <div
+        style={{
+          position: "absolute",
+          width: 400,
+          backgroundColor: "white",
+          padding: 20,
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
       >
-        <div
-          style={{
-            position: "absolute",
-            width: 400,
-            backgroundColor: "white",
-            padding: 20,
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-          }}
+        <Typography variant="h6">{popupMessage}</Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setVerificationPopupOpen(false)}
         >
-          <Typography variant="h6">{popupMessage}</Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => setVerificationPopupOpen(false)}
-          >
-            Close
-          </Button>
-        </div>
-      </Modal>
-      <Modal open={isErrorPopupOpen} onClose={() => setErrorPopupOpen(false)}>
-        <div
-          style={{
-            position: "absolute",
-            width: 400,
-            backgroundColor: "white",
-            padding: 20,
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-          }}
+          Close
+        </Button>
+      </div>
+    </Modal>
+    <Modal open={isErrorPopupOpen} onClose={() => setErrorPopupOpen(false)}>
+      <div
+        style={{
+          position: "absolute",
+          width: 400,
+          backgroundColor: "white",
+          padding: 20,
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <Typography variant="h6">{popupMessage}</Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setErrorPopupOpen(false)}
         >
-          <Typography variant="h6">{popupMessage}</Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => setErrorPopupOpen(false)}
-          >
-            Close
-          </Button>
-        </div>
-      </Modal>
-    </div>
-    </div>
+          Close
+        </Button>
+      </div>
+    </Modal>
+  </div>
   );
 };
 
